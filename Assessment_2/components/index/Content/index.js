0e6f66f414b0afe35a861e8707570a0d3d836361
Henry Hedges <henryhedges1@gmail.com>
2019-02-27 05:31:47 -0600

    added room mapping function, adding initial state function because mac number of rooms don't overlap with initial data (assuming must have 4 total)

--- Assessment_2/components/index/Content/index.js
+++ Assessment_2/components/index/Content/index.js
@@ -4,12 +4,23 @@ import RoomCard from './RoomCard'
 class Content extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = this.createInitialState(this.props.data)
+  }
+
+  createInitialState(data) {
+    const stateIndex = {}
+
+    return stateIndex
+  }
+
+  renderRooms(allRooms = []) {
+    return allRooms.map(room => <RoomCard {...room} />)
   }
   
   render() {
-    return (
-      <RoomCard></RoomCard>
-    )
+    const { maxRooms } = this.props;
+    return this.renderRooms()
   }
 }
 
